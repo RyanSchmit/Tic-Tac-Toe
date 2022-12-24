f5b5b35998b00725e71f8285aa33e34e964b1334
Ryan Sch <ryan@Ryans-Air-34.home>
2022-12-24 00:09:38 -0500

    Deleted typescript file Added mark feature

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-// tsc script.ts --outFile script.js
-var board = document.getElementById('board');
-var board2 = document.getElementById('board2');
-var gameBoard = (function () {
-    var createBoard = function (board) {
-        // create 3 divs with 3 squares in each
-        for (var i = 0; i < 3; i++) {
-            var group = document.createElement('div');
-            group.classList.add('group');
-            board.appendChild(group);
-            for (var i_1 = 0; i_1 < 3; i_1++) {
-                var square = document.createElement('div');
-                square.classList.add('square');
-                group.appendChild(square);
-            }
-        }
-    };
-    return { createBoard: createBoard };
-})();
+const board = document.getElementById('board');
+
+const gameBoard = (() => {
+	const createBoard = (board) => {
+		// create 3 divs with 3 squares in each
+		for (let i = 0; i < 3; i++) {
+			const group = document.createElement('div');
+			group.classList.add('group');
+			board.appendChild(group);
+			for (let i = 0; i < 3; i++) {
+				const square = document.createElement('div');
+				square.classList.add('square');
+				group.appendChild(square);
+				square.addEventListener('click', () => {
+                    const takenSquare = document.createElement('div');
+                    takenSquare.classList.add('taken-square');
+                    square.appendChild(takenSquare);
+                    takenSquare.textContent = 'X';
+				})
+			}
+		}
+	}
+	return { createBoard }
+  })();
+
+
 gameBoard.createBoard(board);
-gameBoard.createBoard(board2);
