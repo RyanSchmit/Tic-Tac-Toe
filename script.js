69eb3378d8d083b5f9b198cd30fb2abd83315bf2
Ryan Sch <ryan@Ryans-MacBook-Air-34.local>
2023-01-07 12:37:06 -0500

    Changed the function to a modules and function factories

--- script.js
+++ script.js
@@ -1,75 +1,110 @@
-// tsc script.ts --outFile script.js
-// create board 
-var board = document.getElementById('board');
-var createBoard = function () {
-    var squares = [];
-    var currentPosition = 0;
-    // create 3 divs with 3 squares in each
-    for (var i = 0; i < 3; i++) {
-        var group = document.createElement('div');
-        group.classList.add('group');
-        board.appendChild(group);
-        for (var i_1 = 0; i_1 < 3; i_1++) {
-            var newSquare = { position: currentPosition, taken: false, player: 0 };
-            squares.push(newSquare);
-            var squareDiv = document.createElement('div');
-            squareDiv.classList.add('square');
-            squareDiv.setAttribute('id', newSquare['position'].toString());
-            group.appendChild(squareDiv);
-            squareDiv.addEventListener('click', playerMarking);
-            currentPosition += 1;
-        }
-    }
-    return squares;
-};
-var squares = createBoard();
-// mark with X or O and change squares object
-var playerOneTurn = true;
-var gameOver = false;
-function playerMarking() {
-    if (squares[this.id]['taken'] || gameOver) {
+var gameBoard = [];
+var GameBoard = (function () {
+    var board = document.getElementById('board');
+    if (board === null) {
         return;
     }
-    var move = document.querySelector('.move');
-    var takenSquare = document.createElement('div');
-    takenSquare.classList.add('taken-square');
-    this.appendChild(takenSquare);
-    if (playerOneTurn) {
-        takenSquare.textContent = 'X';
-        squares[this.id]['player'] = 1;
-        playerOneTurn = false;
-        move.textContent = 'Player 2 move';
-    }
-    else {
-        takenSquare.textContent = 'O';
-        squares[this.id]['player'] = 2;
-        playerOneTurn = true;
-        move.textContent = 'Player 1 move';
-    }
-    squares[this.id]['taken'] = true;
-    checkWin();
-}
-// write function to see if three in a row using position attribute 
-function checkWin() {
-    var winningSquares = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
-    var playerOneSquares = squares.filter(function (s) { return s['player'] === 1; }).map(function (s) { return s['position']; }).sort(function (a, b) { return a - b; });
-    var playerTwoSquares = squares.filter(function (s) { return s['player'] === 2; }).map(function (s) { return s['position']; }).sort(function (a, b) { return a - b; });
-    for (var _i = 0, winningSquares_1 = winningSquares; _i < winningSquares_1.length; _i++) {
-        var winningSquare = winningSquares_1[_i];
-        if (playerOneSquares.includes(winningSquare[0]) && playerOneSquares.includes(winningSquare[1]) && playerOneSquares.includes(winningSquare[2])) {
-            alert("Player One wins");
-            gameOver = true;
+    var createSquare = function (position, taken) {
+        return { position: position, taken: taken };
+    };
+    var createBoard = function () {
+        var currentPosition = 0;
+        // create 3 divs with 3 squares in each
+        for (var i = 0; i < 3; i++) {
+            var group = document.createElement('div');
+            group.classList.add('group');
+            board.appendChild(group);
+            for (var i_1 = 0; i_1 < 3; i_1++) {
+                var newSquare = createSquare(currentPosition, false);
+                gameBoard.push(newSquare);
+                var squareDiv = document.createElement('div');
+                squareDiv.classList.add('square');
+                squareDiv.setAttribute('id', newSquare['position'].toString());
+                group.appendChild(squareDiv);
+                currentPosition += 1;
+            }
         }
-        else if (playerTwoSquares.includes(winningSquare[0]) && playerTwoSquares.includes(winningSquare[1]) && playerTwoSquares.includes(winningSquare[2])) {
-            alert("Player Two wins");
-            gameOver = true;
-        }
-    }
+    };
+    return { createBoard: createBoard };
+})();
+if (GameBoard !== undefined) {
+    GameBoard.createBoard();
 }
-// Clear board for new game 
-// function newGame() {
-// 	board.innerHTML = "";
-// 	const newSquares: Square[] = createBoard();
-// 	console.log(newSquares);
-// 	return newSquares;
-// }
+var Player = function (playerNumber, squares) {
+    return { playerNumber: playerNumber, squares: squares };
+};
+var player1 = Player(1, []);
+var player2 = Player(2, []);
+var GameControl = (function () {
+    var gameOver = false;
+    // const startNewGame = () => {
+    // 	if (GameBoard === undefined) {
+    // 		return
+    // 	}
+    // 	player1 = Player(1, []);
+    // 	player2 = Player(2, []);
+    // 	return gameBoard;
+    // }
+    var playerTurn = function () {
+        var player = document.querySelector('.move');
+        if (player === null) {
+            return 0;
+        }
+        else if (player.textContent == 'Player 1 move') {
+            player.textContent = 'Player 2 move';
+            return 1;
+        }
+        else if (player.textContent == 'Player 2 move') {
+            player.textContent = 'Player 1 move';
+            return 2;
+        }
+    };
+    var checkWin = function () {
+        var winningSquares = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+        var moveText = document.querySelector('.move');
+        if (moveText === null) {
+            return;
+        }
+        var playerOneSquares = player1['squares'].sort(function (a, b) { return a - b; });
+        var playerTwoSquares = player2['squares'].sort(function (a, b) { return a - b; });
+        for (var _i = 0, winningSquares_1 = winningSquares; _i < winningSquares_1.length; _i++) {
+            var winningSquare = winningSquares_1[_i];
+            if (playerOneSquares.includes(winningSquare[0]) && playerOneSquares.includes(winningSquare[1]) && playerOneSquares.includes(winningSquare[2])) {
+                alert("Player One wins");
+                gameOver = true;
+                moveText.textContent = 'Game Over';
+            }
+            else if (playerTwoSquares.includes(winningSquare[0]) && playerTwoSquares.includes(winningSquare[1]) && playerTwoSquares.includes(winningSquare[2])) {
+                alert("Player Two wins");
+                gameOver = true;
+                moveText.textContent = 'Game Over';
+            }
+        }
+    };
+    var markPosition = function () {
+        var squaresDiv = Array.from(document.querySelectorAll('.square'));
+        squaresDiv.forEach(function (square) {
+            square.addEventListener('click', function () {
+                var player = playerTurn();
+                if (gameBoard[parseInt(square.id)]['taken'] || gameOver) {
+                    return;
+                }
+                var takenSquare = document.createElement('div');
+                takenSquare.classList.add('taken-square');
+                square.appendChild(takenSquare);
+                if (player === 1) {
+                    takenSquare.textContent = 'X';
+                    player1['squares'].push(parseInt(square.id));
+                }
+                else {
+                    takenSquare.textContent = 'O';
+                    player2['squares'].push(parseInt(square.id));
+                }
+                gameBoard[parseInt(square.id)]['taken'] = true;
+                checkWin();
+            });
+        });
+    };
+    return { markPosition: markPosition };
+})();
+GameControl.markPosition();
--- script.ts
+++ script.ts
@@ -1,85 +1,135 @@
-// tsc script.ts --outFile script.js
-
 interface Square {
 	position: number,
 	taken: boolean,
-	player: number
 }
 
-// create board 
-const board = document.getElementById('board');
+const gameBoard: Square[] = [];
 
-const createBoard = (): Square[] => {
-	const squares: Square[] = [];
-	let currentPosition: number = 0;
-	// create 3 divs with 3 squares in each
-	for (let i = 0; i < 3; i++) {
-		const group = document.createElement('div');
-		group.classList.add('group');
-		board.appendChild(group);
+const GameBoard = (() => {
+	const board = document.getElementById('board');
+	if (board === null) {
+		return
+	 }
+
+	const createSquare = (position: number, taken: boolean): Square => {
+		return { position, taken }
+	}
+	
+	const createBoard = () => {
+		let currentPosition: number = 0;
+		// create 3 divs with 3 squares in each
 		for (let i = 0; i < 3; i++) {
-			const newSquare: Square = {position: currentPosition, taken: false, player: 0};
-			squares.push(newSquare);
-			const squareDiv = document.createElement('div');
-			squareDiv.classList.add('square');
-			squareDiv.setAttribute('id', newSquare['position'].toString());
-			group.appendChild(squareDiv);
-			squareDiv.addEventListener('click', playerMarking)
-			currentPosition += 1;
+			const group = document.createElement('div');
+			group.classList.add('group');
+			board.appendChild(group);
+			for (let i = 0; i < 3; i++) {
+				const newSquare: Square = createSquare(currentPosition, false)
+				gameBoard.push(newSquare);
+				const squareDiv = document.createElement('div');
+				squareDiv.classList.add('square');
+				squareDiv.setAttribute('id', newSquare['position'].toString());
+				group.appendChild(squareDiv);
+				currentPosition += 1;
+			}
 		}
 	}
-	return squares;
-};
 
-const squares: Square[] = createBoard();
+	return { createBoard }
+})();
 
-// mark with X or O and change squares object
-let playerOneTurn: boolean = true;
-let gameOver: boolean = false;
 
-function playerMarking() {
-	if (squares[this.id]['taken'] || gameOver) {
-		return;
-	}
-	const move = document.querySelector('.move');
-	const takenSquare = document.createElement('div');
-	takenSquare.classList.add('taken-square');
-	this.appendChild(takenSquare);
-	if (playerOneTurn) {
-		takenSquare.textContent = 'X';
-		squares[this.id]['player'] = 1;
-		playerOneTurn = false;
-		move.textContent = 'Player 2 move';
-	} else {
-		takenSquare.textContent = 'O';
-		squares[this.id]['player'] = 2;
-		playerOneTurn = true;
-		move.textContent = 'Player 1 move';
-	}
-	squares[this.id]['taken'] = true;
-	checkWin();
+if (GameBoard !== undefined) {
+	GameBoard.createBoard();
 }
 
-// write function to see if three in a row using position attribute 
-function checkWin() {
-	const winningSquares: number[][] = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-	const playerOneSquares: number[] = squares.filter((s) => s['player'] === 1).map((s) => s['position']).sort((a, b) => a - b);
-	const playerTwoSquares: number[] = squares.filter((s) => s['player'] === 2).map((s) => s['position']).sort((a, b) => a - b);
-	for (const winningSquare of winningSquares) {
-		if (playerOneSquares.includes(winningSquare[0]) && playerOneSquares.includes(winningSquare[1]) && playerOneSquares.includes(winningSquare[2])) {
-			alert("Player One wins");
-			gameOver = true;
-		} else if (playerTwoSquares.includes(winningSquare[0]) && playerTwoSquares.includes(winningSquare[1]) && playerTwoSquares.includes(winningSquare[2])) {
-			alert("Player Two wins");
-			gameOver = true;
+
+interface Player {
+	playerNumber: number,
+	squares: number[]
+}
+
+
+const Player = (playerNumber: number, squares: number[]) => {
+	return { playerNumber, squares }
+}
+
+let player1: Player = Player(1, []);
+let player2: Player = Player(2, []);
+
+const GameControl = (() => {
+	let gameOver: boolean = false;
+	// const startNewGame = () => {
+	// 	if (GameBoard === undefined) {
+	// 		return
+	// 	}
+	// 	player1 = Player(1, []);
+	// 	player2 = Player(2, []);
+	// 	return gameBoard;
+	// }
+
+
+	const playerTurn = (): number => {
+		const player = document.querySelector('.move');
+		if (player === null ) {
+			return 0;
+		} else if (player.textContent == 'Player 1 move') {
+			player.textContent = 'Player 2 move';
+			return 1;
+		} else if (player.textContent == 'Player 2 move') {
+			player.textContent = 'Player 1 move';
+			return 2;
+		} else {
+			return 0;
 		}
 	}
-}
 
-// Clear board for new game 
-// function newGame() {
-// 	board.innerHTML = "";
-// 	const newSquares: Square[] = createBoard();
-// 	console.log(newSquares);
-// 	return newSquares;
-// }
+	const checkWin = () => {
+		const winningSquares: number[][] = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+		const moveText = document.querySelector('.move');
+		if (moveText === null) {
+			return
+		}
+		const playerOneSquares: number[] = player1['squares'].sort((a, b) => a - b);
+		const playerTwoSquares: number[] = player2['squares'].sort((a, b) => a - b);
+		for (const winningSquare of winningSquares) {
+			if (playerOneSquares.includes(winningSquare[0]) && playerOneSquares.includes(winningSquare[1]) && playerOneSquares.includes(winningSquare[2])) {
+				alert("Player One wins");
+				gameOver = true;
+				moveText.textContent = 'Game Over';
+			} else if (playerTwoSquares.includes(winningSquare[0]) && playerTwoSquares.includes(winningSquare[1]) && playerTwoSquares.includes(winningSquare[2])) {
+				alert("Player Two wins");
+				gameOver = true;
+				moveText.textContent = 'Game Over';
+			}
+		}
+	}
+	
+	const markPosition = () => {
+		const squaresDiv = Array.from(document.querySelectorAll('.square'));
+		squaresDiv.forEach((square) => {
+			square.addEventListener('click', () => {
+			const player: number = playerTurn();
+			if (gameBoard[parseInt(square.id)]['taken'] || gameOver) {
+				return;
+			}
+			const takenSquare = document.createElement('div');
+			takenSquare.classList.add('taken-square');
+			square.appendChild(takenSquare);
+			if (player === 1) {
+				takenSquare.textContent = 'X';
+				player1['squares'].push(parseInt(square.id));
+			} else {
+				takenSquare.textContent = 'O';
+				player2['squares'].push(parseInt(square.id));
+			}
+			gameBoard[parseInt(square.id)]['taken'] = true;
+			checkWin();
+			});
+		})
+	}
+
+	return { markPosition }
+})()
+
+
+GameControl.markPosition();
